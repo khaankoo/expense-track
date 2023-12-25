9d8b0b80e0cf2cbf0cb9fca7b18a3580c9f5f739
Khangerel Be-ochir <khangeerelbeochir@gmail.com>
2023-12-25 23:19:59 +0800

    mar2

--- Frontend/expense-front/src/app/page.js
+++ Frontend/expense-front/src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Email from "@/components/Email";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex flex-col mx-auto">
+      <Email />
     </main>
   )
 }
--- Frontend/expense-front/src/components/Email.jsx
+++ Frontend/expense-front/src/components/Email.jsx
@@ -1,4 +1,29 @@
+import Geld from "@/images/Geld";
 
 const Email = () => {
+  return (
+    <div className="flex justify-center items-center text-center">
+      <div className="flex flex-col mx-auto">
+        <div className="flex flex-col mx-auto w-60 gap-4">
+          <Geld />
+          <h1>Welcome Back</h1>
+          <p>Welcome back, Please enter your details</p>
+          <div className="flex flex-col gap-2">
+            <input className="p-2 rounded-lg border-2 border-gray-300" type="email" id="" placeholder="Email"/>
+            <input className="p-2 rounded-lg border-2 border-gray-300" type="password" id="" placeholder="Password"/>
+            <button className="btn bg-blue-600 text-white rounded-3xl">
+              Log in
+            </button>
+          </div>
+          <div className="flex gap-2">
+            <p>Don’t have account?</p>
+            <button className="text-blue-500">Sign up</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex bg-blue-600 h-full w-full">bene</div>
+    </div>
+  );
+};
 
-}
+export default Email;
