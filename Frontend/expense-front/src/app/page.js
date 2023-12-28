50a42e1cd20d50fcd5511af77ffa16a16beecfab
Khangerel Be-ochir <23LP5567@G303-CC0126.local>
2023-12-28 12:02:36 +0800

    frint

--- Frontend/expense-front/src/app/page.js
+++ Frontend/expense-front/src/app/page.js
@@ -1,11 +1,10 @@
-import Currency from "@/components/Currency";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col mx-auto w-full bg-[#eff0f2]">
       <Navbar />
-      <Currency />
+
     </main>
   )
 }
