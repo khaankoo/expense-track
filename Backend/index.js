ea3c8413117926b91e669ba703babb1dfa3f2a1e
Khangerel Be-ochir <23LP5567@G303-CC0126.local>
2024-01-03 12:06:31 +0800

    fixed

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import bp from "body-parser";
-import { pool } from "./db.js";
-import { user } from "./router/user.js";
+import { pool } from "./src/utils/db.js"
+import { user } from "./src/router/user.js";
 
 dotenv.config();
 
@@ -10,27 +10,29 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bp.json());
 
-app.use('/users', user)
+app.use("/users", user);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
 
-
 app.post("/createTable", async (_, res) => {
   try {
     const tableQueryText = `
     CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL
-    )`;
+      id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name VARCHAR(50) NOT NULL,
+      email VARCHAR(50) UNIQUE NOT NULL,
+      password TEXT,
+      avatar_img blob,
+      createdAt TIMESTAMP,
+      updateAt TIMESTAMP,
+      currency_type TEXT DEFAUL ‘MNT’
+      )`;
 
     await pool.query(tableQueryText);
-    res.send('ok');
-
+    res.send("ok");
   } catch (error) {
     console.error(error);
   }
 });
-
